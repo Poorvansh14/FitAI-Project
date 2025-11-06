ea3693ad50c1e702a432dd10c130b9a23b1b9438
Your Name <your_email@example.com>
2025-11-06 12:43:13 +0530

    Switch to gunicorn for production

--- server/app.py
+++ server/app.py
@@ -196,6 +196,6 @@ def update_profile():
 
 
 # ----------------- APP ENTRYPOINT -----------------
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+   ## port = int(os.environ.get("PORT", 5000))
+#app.run(host="0.0.0.0", port=port)
